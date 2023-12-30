a5bc1461cee4d5edc4e57c429e1a4687b05bdfcf
Tran Thanh Sum <39401951+ThanhSum@users.noreply.github.com>
2023-12-30 21:00:22 +0700

    Update messageBoard.sol

--- helloWorld.sol
+++ helloWorld.sol
@@ -1,15 +1,44 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.7;
+pragma solidity ^0.8.7;
 
-contract HelloWorld {
-    string public message;
-
-    constructor(string memory initialMessage) {
-        message = initialMessage;
+contract MessageBoard {
+    struct Post {
+        string content;
+        uint256 timestamp;
+        address author;
     }
 
-    function updateMessage(string memory newMessage) public {
-        message = newMessage;
+    mapping(uint256 => Post) public postHistory;
+    uint256 public postCount;
+
+    address public owner;
+
+    event PostUpdated(string newPostContent, address updatedBy, uint256 timestamp);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can perform this action");
+        _;
+    }
+
+    constructor(string memory initialPostContent) {
+        postCount = 1;
+        postHistory[postCount] = Post(initialPostContent, block.timestamp, msg.sender);
+        owner = msg.sender;
+    }
+
+    function updatePost(string memory newPostContent) public {
+        require(bytes(newPostContent).length > 0, "Post should not be empty");
+        postCount++;
+        postHistory[postCount] = Post(newPostContent, block.timestamp, msg.sender);
+        emit PostUpdated(newPostContent, msg.sender, block.timestamp);
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "Invalid new owner address");
+        address previousOwner = owner;
+        owner = newOwner;
+        emit OwnershipTransferred(previousOwner, newOwner);
     }
 }
